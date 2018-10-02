4912496fd25d51de04ca6107b9126511cd6d0c03
204maker <34336298+204maker@users.noreply.github.com>
2018-10-01 20:05:21 -0500

    Update Duck.java

--- Oct1/Duck.java
+++ Oct1/Duck.java
@@ -1,17 +1,23 @@
 
 //----------------------------------------------------------------------------------
-//  Exercise 3: 
+//  Exercise: 
 //  OOP example on Inheritance
+//    - Inheritance works only with one parent
+//    - 
 //----------------------------------------------------------------------------------
 
-public class Duck extends Animal
+public class Duck extends FlyAnimal
 {
-  int color;
-  int age;
-  int weight;
-  String specie;
+  private Duck friend;               // One duck can have another friend of type duck
+                                     // JVM sees principal and a friend - example of AGGREGATION
+ 
+  public Duck(int age, int color, String specie)
+  {
+    super(age, color, specie);      // superclass
+  }
 
 
+  // void eat(int food)
   void eat(int food)
   {
     if(weight+food < 20000)
@@ -23,12 +29,19 @@ public class Duck extends Animal
     else
       System.out.println("Duck is full!");
   }
+  
 
+  public void walk()
+  {
+    System.out.println("Walking...........done!");
+  }
 
+  // void fly(int limit)
   void fly(int limit)
   {
     System.out.println("Flying over "+limit+" meters.");
   }
+  
 }
 
 
